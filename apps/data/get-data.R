bf865ac1df199797e74b4f01e1966e5c6ca9d2d6
Lesia <brusentcevva@gmail.com>
2023-02-12 23:55:31 +0200

    choose country data integrated in dash-boardd ui/server second block

--- apps/data/get-data.R
+++ apps/data/get-data.R
@@ -1,3 +1,4 @@
 
-load("~/dash-board/apps/data/WDI_Countries.Rda")
-load("~/dash-board/apps/data/WDI_Indicators.Rda")
+load("~/dash-boardd/apps/data/WDI_Countries.Rda")
+load("~/dash-boardd/apps/data/WDI_Indicators.Rda")
+
