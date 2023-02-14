dbfeef8ab34ade962836a5bfdbd9f387ec44af1e
michael-aksonov <maksonov@kse.org.ua>
2023-02-14 02:26:55 +0200

    Added Country map+list app (not yet module)

--- apps/choose-country-map/choose-country-map.R
+++ apps/choose-country-map/choose-country-map.R
@@ -1,9 +1,10 @@
 ui <- fluidPage(
-  tags$head(tags$style(HTML("
+  tags$head(tags$style(
+    HTML("
     .leaflet-container {
     background-color:rgba(255,0,0,0.0);}
-    "))
-  ),
+    ")
+  )),
   theme = bs_theme(version = 4, bootswatch = "minty"),
   shinydashboard::box(
     width = 12,
@@ -13,10 +14,11 @@ ui <- fluidPage(
         inputId = 'searchCountry',
         label = strong('Select Country'),
         choices = WDI_Countries$Country.Name,
-        selected = c("Ukraine","United States"),
+        selected = c("Ukraine", "United States"),
         multiple = TRUE,
         # allow for multiple inputs
-        options = list(create = FALSE))
+        options = list(create = FALSE)
+      )
     ),
     column(width = 10,
            leafletOutput(outputId = "myMap",
@@ -28,21 +30,23 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   foundational.map <- reactive({
     leaflet() %>%
-      addPolygons(data = WB_CountryPolygons,
-                  layerId = WB_CountryPolygons$NAME_EN,
-                  group = "click.list", 
-                  fill = TRUE,
-                  fillColor = "#f8f4f4",
-                  fillOpacity = 1,
-                  weight=1,
-                  color = "#5a5a5a",
-                  highlightOptions = highlightOptions(weight= 4,
-                                                      color = "#80c4ac",
-                                                      bringToFront = T
-                                                      ),
-                  label = as.character(WB_CountryPolygons$NAME_EN),
-                  labelOptions = labelOptions(noHide = FALSE)
-                  
+      addPolygons(
+        data = WB_CountryPolygons,
+        layerId = WB_CountryPolygons$NAME_EN,
+        group = "click.list",
+        fill = TRUE,
+        fillColor = "#f8f4f4",
+        fillOpacity = 1,
+        weight = 1,
+        color = "#5a5a5a",
+        highlightOptions = highlightOptions(
+          weight = 4,
+          color = "#80c4ac",
+          bringToFront = T
+        ),
+        label = as.character(WB_CountryPolygons$NAME_EN),
+        labelOptions = labelOptions(noHide = FALSE)
+        
       )
   })
   
@@ -51,9 +55,7 @@ server <- function(input, output, session) {
     
   })
   
-  output$text = reactive({WDI_Countries[WDI_Countries$Country.Name %in% input$searchCountry, 2]})
-  
-  click.list <- reactiveValues(ids = vector()) 
+  click.list <- reactiveValues(ids = vector())
   
   # observe where the user clicks on the leaflet map
   # during the Shiny app session
@@ -62,12 +64,11 @@ server <- function(input, output, session) {
   # https://rstudio.github.io/leaflet/shiny.html
   
   observeEvent(input$myMap_shape_click, {
-    
     # store the click(s) over time
-    click <- input$myMap_shape_click
+    click <- c(input$myMap_shape_click, input$searchCountry)
     
     # store the polygon ids which are being clicked
-    click.list$ids <- c(click.list$ids, click$id) 
+    click.list$ids <- c(click.list$ids, click$id)
     
     # filter the spatial data frame
     # by only including polygons
@@ -87,21 +88,34 @@ server <- function(input, output, session) {
           fill = TRUE,
           fillColor = "#f8949c",
           fillOpacity = 1,
-          weight= 4,
+          weight = 4,
           color = "#80c4ac"
         )
-      updateSelectizeInput(session,"searchCountry", selected = c(input$searchCountry,click.list$ids))
+      updateSelectizeInput(session,
+                           "searchCountry",
+                           selected = c(input$searchCountry, click.list$ids))
     }
   })
-
   
-  observeEvent(input$clearHighlight, {
-    output$myMap <- renderLeaflet({
-      click.list$ids <- NULL
-      foundational.map()
-      
-    })
-    
+  observeEvent(input$searchCountry,
+               {
+                 lines.of.interest <-
+                   WB_CountryPolygons[which(WB_CountryPolygons$NAME_EN %in% input$searchCountry) ,]
+                 
+                 leafletProxy(mapId = "myMap") %>%
+                   addPolygons(
+                     data = lines.of.interest,
+                     layerId = lines.of.interest@data$id,
+                     fill = TRUE,
+                     fillColor = "#f8949c",
+                     fillOpacity = 1,
+                     weight = 4,
+                     color = "#80c4ac"
+                   )
+               })
+  
+  output$text = reactive({
+    WDI_Countries[WDI_Countries$Country.Name %in% input$searchCountry, 2]
   })
   
 }
