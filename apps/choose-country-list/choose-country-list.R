6bd8997b02e80ebf49549b74c99e1f2baea1fdf4
michael-aksonov <maksonov@kse.org.ua>
2023-02-18 00:02:46 +0200

    Forbit to chode multiple countries

--- apps/choose-country-list/choose-country-list.R
+++ apps/choose-country-list/choose-country-list.R
@@ -2,20 +2,20 @@ ChooseCountryListUI <- function(id) {
   ns <- NS(id)
   fluidPage(
     theme = bs_theme(version = 4, bootswatch = "minty"),
-    fluidRow(
+    fluidRow( 
       selectizeInput(
         inputId = ns('searchCountry'),
         label = strong('Select Country'),
         choices = WDI_Countries$Country.Name,
         selected = c("Ukraine","United States"),
-        multiple = TRUE,
-        # allow for multiple inputs
-        options = list(create = FALSE) # if TRUE, allows newly created inputs
+        multiple = FALSE,
+        options = list(create = FALSE),
+        width = "100%"
       )
     ),
     fluidRow(
       helpText(
-        "Start typing country or region name or select it on the map. You can select any number of countries. To clear your selection use Backspace to delete the last selected country, chose the country from the list and delete it, or clear all selection "
+        "Start typing country or region name. You may select <strong>only one</strong> country"
       )
     ),
     hr()
